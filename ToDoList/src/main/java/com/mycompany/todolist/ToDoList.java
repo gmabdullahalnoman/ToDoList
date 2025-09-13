ae236aa2a8b6976841ddb87f3f23b417742b530e
Noman <gmabdullahalnoman@gmail.com>
2025-09-13 19:17:32 +0600

    Initial commit

---
+++ ToDoList/src/main/java/com/mycompany/todolist/ToDoList.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.todolist;
+
+/**
+ *
+ * @author abdullahalnoman
+ */
+public class ToDoList {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
