fee23bc1c4bd46198e0195bb113a37db151c40ab
Noman <gmabdullahalnoman@gmail.com>
2025-09-14 20:24:36 +0600

    addTask and viewTask method added

--- ToDoList/src/main/java/com/mycompany/todolist/ToDoList.java
+++ ToDoList/src/main/java/com/mycompany/todolist/ToDoList.java
@@ -1,27 +1,32 @@
 package com.mycompany.todolist;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 /**
  *
  * @author abdullahalnoman
  */
 public class ToDoList {
+    List<Task> tasks = new ArrayList<>();
+    Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        ToDoList todo = new ToDoList();
+        
         while (true) {
-            System.out.println("\n--- ToDo List Menu ---");
+            System.out.println("\n>>>>> ToDo List Menu <<<<<");
             System.out.println("1. Add Task");
             System.out.println("2. View Tasks");
             System.out.println("3. Update Task Status");
             System.out.println("4. Exit");
             System.out.print("Choose option: ");
             
-            int choice = input.nextInt();
-            input.nextLine(); 
+            int choice = todo.input.nextInt();
+            todo.input.nextLine(); 
 
             switch (choice) {
-                case 1 -> System.out.println("Add Task");
-                case 2 -> System.out.println("View Tasks");
+                case 1 -> todo.addTask();
+                case 2 -> todo.viewTasks();
                 case 3 -> System.out.println("Update Task Status");
                 case 4 -> {
                     System.out.println("Goodbye!");
@@ -30,6 +35,26 @@ public class ToDoList {
                 default -> System.out.println("Invalid choice.");
             }
         }
+    }
+    void addTask() {
+        System.out.println("Enter task description: ");
+        String description = input.nextLine();
+        tasks.add(new Task(description));
+        System.out.println("Task added");
+    }
+    
+    void viewTasks() {
+        if (tasks.isEmpty()){
+            System.out.println("No tasks added yet.");
+            return;
+        }
+        System.out.println("\n--- Your Tasks ---");
+        for(int i=0; i<tasks.size();i++){
+            System.out.println((i+1) + ". " + tasks.get(i));
+        }
+    }
+    void updateTaskStatus() {
+        // we'll complete this next day.
     }
 }
     
